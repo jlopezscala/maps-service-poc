839910b27a9badfe6b4adb6f6cdf575e181372f2
Julian Scala <julianlopezscala@gmail.com>
2022-05-16 17:15:56 -0300

    feat(DB): Test PostGIS extension on Postgress with Alembic

--- alembic/versions/b6e0925ba7f2_create_polygons_table.py
+++ alembic/versions/b6e0925ba7f2_create_polygons_table.py
@@ -9,6 +9,8 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import ForeignKey
 from sqlalchemy.dialects import postgresql
+from geoalchemy2.types import Geometry
+
 # revision identifiers, used by Alembic.
 revision = 'b6e0925ba7f2'
 down_revision = '8f25b4c94aa7'
@@ -21,14 +23,10 @@ def upgrade():
         'polygons',
         sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True),
         sa.Column('name', sa.String(50), nullable=False),
-        sa.Column('description', sa.Unicode(200)),
+        sa.Column('project_id', postgresql.UUID(as_uuid=True), ForeignKey('projects.id', ondelete='CASCADE')),
+        sa.Column('geom', Geometry('POLYGON'))
     )
 
-    op.create_table(
-        'polygonsxprojects',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('polygon_id', postgresql.UUID(as_uuid=True), ForeignKey('polygons.id')),
-        sa.Column('project_id', postgresql.UUID(as_uuid=True), ForeignKey('projects.id')),)
 
 def downgrade():
     op.drop_table('polygons')
